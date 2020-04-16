aa0d9e4fb18293d806ea339dff049d7b6947ae1f
plunes <plunestechnologies@gmail.com>
2020-04-16 22:57:03 +0530

    Added

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: 5001,
     JWT_KEY: 'Make my day!',
-    MONGODB_URL: 'mongodb://postman_assignment:9413384569Av@ds129610.mlab.com:29610/heroku_0rj0q7qt',
+    MONGODB_URL: 'mongodb://postman_assignment:9413384569Av@ds129610.mlab.com:29610/heroku_4rt9298w',
 }
 //mongodb://<dbuser>:<dbpassword>@ds129610.mlab.com:29610/heroku_0rj0q7qt
